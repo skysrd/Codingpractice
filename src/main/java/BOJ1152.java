7cfa111111f990f2c3e1f08209ffb62714f5ff25
skysrd <37787079+skysrd@users.noreply.github.com>
2021-09-06 11:09:06 +0900

    [FIX]

--- src/main/java/BOJ1152.java
+++ src/main/java/BOJ1152.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BOJ1152 {
+import java.util.Scanner;
+
+    public class BOJ1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        int result=1;
+
+        for (int i=1; i<string.length()-1; i++) {
+            if (string.charAt(i) == ' '&&string.charAt(i+1)!=' ') {
+                result++;
+            }
+
+        }
+        System.out.println(result);
+    }
 }
