29ac697905dcdc86bc35ca4ad7cde789ff82df32
skysrd <37787079+skysrd@users.noreply.github.com>
2021-09-06 11:19:36 +0900

    [SOLVED] BOJ10951

--- src/main/java/BOJ10950.java
+++ src/main/java/BOJ10950.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class BOJ10950 {
+import java.util.Scanner;
+
+public class BOJ10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int cases = scanner.nextInt();
+
+        for(int i=0; i<cases; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            System.out.println(A+B);
+        }
+    }
 }
--- src/main/java/BOJ10951.java
+++ src/main/java/BOJ10951.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BOJ10951 {
+import java.util.Scanner;
+
+public class BOJ10951 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextInt()) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            if(A==0&&B==0) {
+                break;
+            }
+
+            System.out.println(A+B);
+        }
+    }
 }
