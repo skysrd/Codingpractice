20b15d6becb81aba5946f8c5a5e4d46edda6316c
skysrd <37787079+skysrd@users.noreply.github.com>
2021-09-07 11:26:56 +0900

    FOR MERGE]

--- src/main/java/BOJ1181.java
+++ src/main/java/BOJ1181.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BOJ1181 {
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int cases = scanner.nextInt();
+
+        String[] array = new String[cases+1];
+        array[cases+1]="0";
+        for(int i=0; i<cases; i++) {
+            array[i]=scanner.next();
+        }
+
+        Arrays.sort(array);
+
+        for(int i=1; i<=array.length; i++) {
+            if(array[i]==array[i-1]);
+            else {
+                System.out.println(array[i]);
+            }
+        }
+    }
 }
