8e965195dffdac1272c05462f185eb67d60a066f
skysrd <37787079+skysrd@users.noreply.github.com>
2021-11-03 16:50:38 +0900

    [SOLVED] Programmers12937

---
+++ src/main/cpp/Programmers12937.cpp
@@ -0,0 +1,13 @@
+// 홀수와 짝수
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "Odd";
+    if(num%2==0) {
+        answer="Even";
+    }
+    return answer;
+}
