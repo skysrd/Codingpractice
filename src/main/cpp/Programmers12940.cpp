77259978569caaf1d14a1af31ad409bb7038556e
skysrd <37787079+skysrd@users.noreply.github.com>
2021-11-03 16:48:54 +0900

    [SOLVED] Programmers12930

---
+++ src/main/cpp/Programmers12940.cpp
@@ -0,0 +1,35 @@
+//최대공약수와 최소공배수
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int gcd (int a, int b) {
+    if(a<b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    while(b!=0) {
+        int c = a%b;
+        a=b;
+        b=c;
+    }
+
+    return a;
+}
+
+int lcm (int a, int b) {
+    return (a*b)/gcd(a,b);
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+
+    answer.push_back(gcd(n,m));
+    answer.push_back(lcm(n,m));
+
+    return answer;
+}
