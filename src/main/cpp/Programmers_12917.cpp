27a374943ac37f89765a7a9a7cdb79632f14b43c
skysrd <37787079+skysrd@users.noreply.github.com>
2021-11-11 14:15:57 +0900

    [SOLVED] Programmers12917

---
+++ src/main/cpp/Programmers_12917.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    vector<char> vec;
+
+    for(int i=0; i<s.size(); i++) {
+        vec.push_back(s[i]);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    reverse(vec.begin(), vec.end());
+
+    for(int i=0; i<vec.size(); i++) {
+        answer+=vec[i];
+    }
+
+
+    return answer;
+}
