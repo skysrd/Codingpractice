9fa8a77869713f089eaee92689d98c6ead2bfc30
skysrd <37787079+skysrd@users.noreply.github.com>
2021-11-16 13:36:27 +0900

    [SOLVED] Programmers12912

---
+++ src/main/cpp/Programmers12912.cpp
@@ -0,0 +1,23 @@
+// 두 정수 사이의 합
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+
+
+    if(a<=b) {
+    for(int i=a; i<=b; i++) {
+        answer+=i;
+    }
+    }
+    else {
+        for(int i=b; i<=a; i++) {
+            answer+=i;
+        }
+    }
+    return answer;
+}
